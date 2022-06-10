5043096d3d66ee1116772b4fb2a757c7d17373af
Moringa <alinoorshukri8@gmail.com>
2022-06-10 08:42:31 +0300

    commit tree 3b49d38f45f41560aa2e21656c2c7677875e8a45

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    const copyCats = [...cats, "Broom"];
+    return copyCats;
+}
+
+function prependCat(name) {
+    const copyCats = ["Arnold", ...cats];
+    return copyCats;
+}
+
+function removeLastCat() {
+    const copyCats = cats.slice(0, -1);
+    return copyCats;
+}
+
+function removeFirstCat() {
+    const copyCats = cats.slice(1);
+    return copyCats;
+}
+
